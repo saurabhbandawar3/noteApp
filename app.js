267b14bd50ae47577fa2a87e8a5511d923d41f85
Saurabh Bandawar <saurabhbandawar3@gmail.com>
2018-08-29 18:19:30 -0400

    removed code

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 console.log('Starting App..');
-const notes = require('./notes.js')
+
 const fs = require('fs');
-const os = require('os');
+const _= require ('lodash');
 
-// var user = os.userInfo();
-// //console.log(user);
-
-// fs.appendFile('notes.txt',`Hello ${user.username}! You are ${notes.age}`, function(err){
-//     if(err){
-//         console.log('Unable to write to file');
-//     }
-// });
-
-var res = notes.add(3,4);
-console.log(res);
-
+const notes = require('./notes.js');
 
--- notes.js
+++ notes.js
@@ -1,10 +1,4 @@
 console.log('Starting notes.js...');
 
-module.exports.addNote= ()=> {
-    console.log('addNote');
-    return 'note';
-};
-
-module.exports.add = (a,b) => {
-    return a+b;
+module.exports.addNote = () => {
 };
